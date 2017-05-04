37938da3e3f37561d906cb213a96ddedb42fa5e8
AmbroiseS <amb.soullier@gmail.com>
2017-05-05 00:22:51 +0200

    Cleaned old code

--- prototype/maquette-android/app/src/main/java/com/example/sikanla/maquettehandi/ScheduledFragment.java
+++ prototype/maquette-android/app/src/main/java/com/example/sikanla/maquettehandi/ScheduledFragment.java
@@ -6,21 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.sikanla.maquettehandi.network.AllRequest;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static java.lang.String.valueOf;
-
 /**
  * Created by Sikanla on 13/02/2017.
  */
 
 public class ScheduledFragment extends Fragment {
 
-    private double lat;
-    private double lng;
 
     public ScheduledFragment() {
     }
@@ -38,46 +29,7 @@ public class ScheduledFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstance) {
         super.onActivityCreated(savedInstance);
-        //example of AllRequest use
-        Map<String,String> parameters = new HashMap<>();
-        parameters.put("email","bob@mail.com");
-        parameters.put("password","bob");
-        Thread thread = new AllRequest(getActivity(),parameters,"/login");
-        thread.start();
 
-/*
-        LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (location != null) {
-            lat = location.getLongitude();
-            lng = location.getLatitude();
-
-        }
-
-        final LocationListener locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                lat = location.getLongitude();
-                lng = location.getLatitude();
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
-        */
 
     }
 
