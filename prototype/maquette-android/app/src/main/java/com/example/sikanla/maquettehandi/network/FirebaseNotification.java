a21935b239a8d8db040a90050a67d1811709b9c2
AmbroiseS <amb.soullier@gmail.com>
2017-05-08 01:47:57 +0200

    Added firebase notificatoion and messaging service

---
+++ prototype/maquette-android/app/src/main/java/com/example/sikanla/maquettehandi/network/FirebaseNotification.java
@@ -0,0 +1,24 @@
+package com.example.sikanla.maquettehandi.network;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Sikanla on 08/05/2017.
+ */
+
+public class FirebaseNotification extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("notif", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        // todo sendRegistrationToServer(refreshedToken);
+    }}
