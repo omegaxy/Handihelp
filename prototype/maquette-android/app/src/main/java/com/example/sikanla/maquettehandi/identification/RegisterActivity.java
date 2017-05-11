9a389538910895e98d8cb7b59b47072b06f7b744
AmbroiseS <amb.soullier@gmail.com>
2017-05-11 02:52:49 +0200

    Corrected Glitches in register

--- prototype/maquette-android/app/src/main/java/com/example/sikanla/maquettehandi/identification/RegisterActivity.java
+++ prototype/maquette-android/app/src/main/java/com/example/sikanla/maquettehandi/identification/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.sikanla.maquettehandi.identification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,44 +31,37 @@ import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText surnameEditText, firstnameEditText, birthYearEditText, mailEditText, passwordEditText1, passwordEditText2, phoneNumberEditText;
-    private Button registrationButton;
+    private Button registerButton;
     private TextView text;
     private String surname, firstname, birthYear, mail, password1, password2, phoneNumber;
-    private boolean registerOk = true;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        User user = new User();
-
         setContentView(R.layout.registration_screen);
 
         surnameEditText = (EditText) findViewById(R.id.surname);
-
         firstnameEditText = (EditText) findViewById(R.id.firstname);
-
         birthYearEditText = (EditText) findViewById(R.id.birthYear);
-
         mailEditText = (EditText) findViewById(R.id.mail);
-
         passwordEditText1 = (EditText) findViewById(R.id.password1);
-
         passwordEditText2 = (EditText) findViewById(R.id.password2);
-
         phoneNumberEditText = (EditText) findViewById(R.id.phoneNumber);
-
-        registrationButton = (Button) findViewById(R.id.registrationButton);
-
+        registerButton = (Button) findViewById(R.id.registrationButton);
         text = (TextView) findViewById(R.id.errorMsg);
+       // text.setVisibility(View.GONE);
 
+        registerButton.setEnabled(false);
 
-        registrationButton.setOnClickListener(new View.OnClickListener() {
+        startTextWatchers();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                //registrationButton.setEnabled(false);
+                //registerButton.setEnabled(false);
 
                 surname = surnameEditText.getText().toString();
                 firstname = firstnameEditText.getText().toString();
@@ -75,100 +70,84 @@ public class RegisterActivity extends AppCompatActivity {
                 password1 = passwordEditText1.getText().toString();
                 password2 = passwordEditText2.getText().toString();
                 phoneNumber = phoneNumberEditText.getText().toString();
-
-
-                if (surname.equals("") || firstname.equals("") || birthYear.equals("") || mail.equals("") || password1.equals("") || password2.equals("") || phoneNumber.equals("")) {
-                    text.setText("Un des champs à saisir est vide");
-                    return;
-                }
-
-                if(!(isInteger(birthYear))){
-                    text.setText("Votre année de naissance doit être un nombre entier");
-                    return;
-                }
-
-                if(!(isbirthYearValid(Integer.parseInt(birthYear)))){
-                    text.setText("Votre année de naissance doit être compris entre 1850 et 2100");
-                    return;
-                }
-
-                if(!(isEmailFormatValid(mail))){
-                    text.setText("Le champ email ne correspond pas à une adresse mail");
-                    return;
-                }
-
-                if(!(isPasswordValid(password1))){
-                    text.setText("Le 1er champ mot de passe doit contenir au minimum 6 caractères");
-                    return;
-                }
-
-                if(!(password1.equals(password2))){
-                    text.setText("Les deux mot de passe ne sont pas identiques");
-                    return;
-                }
-
-                if(!(isPhoneNumberCorrect(phoneNumber))){
-                    text.setText("Le numéro doit être rentrer sous la forme 0612345678");
-                    return;
-                }
-
                 connectToServer(firstname, surname, mail, password1, phoneNumber, birthYear);
-                if(registerOk){
-                    //text.setText("true");
-                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                }
+
 
             }
         });
 
     }
 
-    //year of birth filter
-    private boolean isbirthYearValid(int birthYear){
-        if(birthYear<1850 || birthYear > 2100){
+
+    private boolean areInputsValids() {
+        if (firstnameEditText.getText().toString().isEmpty() || surnameEditText.getText().toString().isEmpty()) {
             return false;
+        } else if (!isEmailValid(mailEditText.getText().toString()) && !mailEditText.getText().toString().isEmpty()) {
+            Log.e("mail", mailEditText.getText().toString());
+            text.setText("Le champ email ne correspond pas à une adresse mail");
+            text.setVisibility(View.VISIBLE);
+            return false;
+        } else if (!isPasswordValid(passwordEditText1.getText().toString()) && !passwordEditText1.getText().toString().isEmpty()) {
+            text.setText("Le mot de passe doit contenir au minimum 6 caractères");
+            text.setVisibility(View.VISIBLE);
+            return false;
+        } else if (!arePasswordSimilar(passwordEditText1.getText().toString(), passwordEditText2.getText().toString())
+                && !passwordEditText1.getText().toString().isEmpty()
+                && !passwordEditText2.getText().toString().isEmpty()) {
+            text.setText("Les deux mot de passe ne sont pas identiques");
+            text.setVisibility(View.VISIBLE);
+            return false;
+        } else if (!birthYearEditText.getText().toString().isEmpty()
+                && !isbirthYearValid(Integer.parseInt(birthYearEditText.getText().toString()))
+                ) {
+            text.setText("Année de naissance comprise entre 1910 et 2017");
+            text.setVisibility(View.VISIBLE);
+            return false;
+        } else if (!isPhoneNumberCorrect(phoneNumberEditText.getText().toString())
+                && !phoneNumberEditText.getText().toString().isEmpty()) {
+            text.setText("Le numéro doit être rentré sous la forme 0612345678");
+            text.setVisibility(View.VISIBLE);
+            return false;
+        } else if (mailEditText.getText().toString().isEmpty() || passwordEditText1.getText().toString().isEmpty()
+                || passwordEditText2.getText().toString().isEmpty() || birthYearEditText.getText().toString().isEmpty()
+                || phoneNumberEditText.getText().toString().isEmpty()) {
+
+            text.setText("");
+           // text.setVisibility(View.GONE);
+            return false;
+
+
         }
-        return true;
-    }
-
-    //verify year of birth type
-    static boolean isInteger(String s) {
-        try{ Integer.parseInt(s); }
-        catch(NumberFormatException nfe){ return false; }
+        text.setText("");
         return true;
     }
 
 
-    //verify identic mails
-    boolean isEmailFormatValid(CharSequence email) {
+    private boolean arePasswordSimilar(String password1, String password2) {
+        return password1.equals(password2);
+    }
+
+    //year of birth filter
+    private boolean isbirthYearValid(int birthYear) {
+        return birthYear > 1909 && birthYear < 2018;
+    }
+
+    boolean isEmailValid(CharSequence email) {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
-    private boolean isPasswordValid(String password)
-    {
+    private boolean isPasswordValid(String password) {
         return password.length() > 5;
     }
 
     //verify phonenumber type
     private boolean isPhoneNumberCorrect(String phoneNumber) {
-
         Pattern pattern = Pattern.compile("((06)|(07))[0-9]{8}");
         Matcher matcher = pattern.matcher(phoneNumber);
-        //"(0|(\\+33)|(0033))[1-9][0-9]{8}"
-
-        if (matcher.matches())
-            return true;
-
-        return false;
+        return matcher.matches();
     }
 
-    //envoyer les données au serveur
-    //vérifier que le mail donné n'est pas déjà pris
-    //bouton s'inscrire -> redirection vers questionnaire ?
 
-
-    //verify mail not already taken
     private void connectToServer(String firstname, String surname, String email,
                                  String password, String phoneNumber, String birth_year) {
 
@@ -191,9 +170,8 @@ public class RegisterActivity extends AppCompatActivity {
                     JSONObject jsonObject = new JSONObject(response);
 
                     if (response.contains("Sorry, this email already exists")) {
-                        //todo manage error message because email already exists
-                        text.setText("mail déjà enregistré");
-                        registerOk = false;
+
+                        text.setText("Email déjà enregistré");
 
                     } else if (jsonObject.get("error").toString() == "false") {
                         text.setText("Inscription acceptée");
@@ -206,8 +184,6 @@ public class RegisterActivity extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
                     } else {
-                        // todo display error because register failed
-                        registerOk = false;
                         text.setText("Inscription refusée");
 
                         // loginButton.setEnabled(true); //reactivate button
@@ -217,6 +193,142 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void startTextWatchers() {
+        firstnameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!areInputsValids()) {
+                    registerButton.setEnabled(false);
+                } else {
+                    registerButton.setEnabled(true);
+                }
+            }
+        });
+        surnameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!areInputsValids()) {
+                    registerButton.setEnabled(false);
+                } else {
+                    registerButton.setEnabled(true);
+                }
+            }
+        });
+        birthYearEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!areInputsValids()) {
+                    registerButton.setEnabled(false);
+                } else {
+                    registerButton.setEnabled(true);
+                }
+            }
+        });
+        mailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!areInputsValids()) {
+                    registerButton.setEnabled(false);
+                } else {
+                    registerButton.setEnabled(true);
+                }
+            }
+        });
+        passwordEditText1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!areInputsValids()) {
+                    registerButton.setEnabled(false);
+                } else {
+                    registerButton.setEnabled(true);
+                }
+            }
+        });
+        passwordEditText2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!areInputsValids()) {
+                    registerButton.setEnabled(false);
+                } else {
+                    registerButton.setEnabled(true);
+                }
+            }
+        });
+        phoneNumberEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!areInputsValids()) {
+                    registerButton.setEnabled(false);
+                } else {
+                    registerButton.setEnabled(true);
                 }
             }
         });
