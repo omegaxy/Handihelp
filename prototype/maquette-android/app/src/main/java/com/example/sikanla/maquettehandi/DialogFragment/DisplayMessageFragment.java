e5676d56ddf385b51e125eaa5e3372486c520bdc
AmbroiseS <amb.soullier@gmail.com>
2017-05-27 01:07:56 +0200

    Added send Button to messages

--- prototype/maquette-android/app/src/main/java/com/example/sikanla/maquettehandi/DialogFragment/DisplayMessageFragment.java
+++ prototype/maquette-android/app/src/main/java/com/example/sikanla/maquettehandi/DialogFragment/DisplayMessageFragment.java
@@ -2,17 +2,17 @@ package com.example.sikanla.maquettehandi.DialogFragment;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.example.sikanla.maquettehandi.Adapters.ContactListAdapter;
 import com.example.sikanla.maquettehandi.Adapters.DisplayMessageAdapter;
-import com.example.sikanla.maquettehandi.Model.Contact;
 import com.example.sikanla.maquettehandi.Model.Message;
 import com.example.sikanla.maquettehandi.R;
 import com.example.sikanla.maquettehandi.network.MessageRequester;
@@ -28,22 +28,56 @@ public class DisplayMessageFragment extends DialogFragment {
     private ListView listView;
     private TextView textViewName;
     private ArrayList<Message> arrayOfMessages;
+    private EditText textTosend;
+    private Button sendText;
+    private MessageRequester messageRequester;
+    String idContact;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.df_display_message, container, false);
-        arrayOfMessages= null;
         arrayOfMessages = new ArrayList<Message>();
         displayMessageAdapter = new DisplayMessageAdapter(getActivity(), arrayOfMessages);
         listView = (ListView) view.findViewById(R.id.lv_display_message);
+        textTosend = (EditText) view.findViewById(R.id.df_display_message_edittext_send);
+        sendText = (Button) view.findViewById(R.id.df_display_message_button_send);
+        messageRequester = new MessageRequester();
+
+        idContact = getArguments().getString("id");
+        
+        sendText.setEnabled(false);
+        sendText.setAlpha(0.5f);
+        sendText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendText();
+            }
+        });
+
+
         textViewName = (TextView) view.findViewById(R.id.df_display_message_textview_name);
         textViewName.setText(getArguments().getString("firstname"));
-        String id = getArguments().getString("id");
-        fetchMessages(id);
+        fetchMessages(idContact);
+        sendButtonBehaviour();
         return view;
     }
 
+
+    private void sendText() {
+
+        messageRequester.sendMessage(getActivity(), textTosend.getText().toString(), idContact, new MessageRequester.SendMessagesCB() {
+            @Override
+            public void onMessageReceived(Boolean success) {
+                if (success) {
+                    displayMessageAdapter.add(new Message(textTosend.getText().toString(), true));
+                    textTosend.setText("");
+
+                }
+            }
+        });
+    }
+
+
     private void fetchMessages(String id) {
-        MessageRequester messageRequester = new MessageRequester();
         messageRequester.getMessage(getActivity(), id, new MessageRequester.GetMessagesCB() {
             @Override
             public void onMessagesReceived(ArrayList<Message> arrayList, Boolean success) {
@@ -56,5 +90,32 @@ public class DisplayMessageFragment extends DialogFragment {
         });
     }
 
+    private void sendButtonBehaviour() {
+        textTosend.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (textTosend.getText().toString().matches("")) {
+                    sendText.setEnabled(false);
+                    sendText.setAlpha(0.5f);
+                } else {
+                    sendText.setAlpha(1f);
+                    sendText.setEnabled(true);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+    }
 }
+
