c5c8fac282df316c1fa3732849758e642e6f052e
AmbroiseS <amb.soullier@gmail.com>
2017-06-14 14:44:43 +0200

    added respond to instant

--- prototype/maquette-android/app/src/main/java/com/example/sikanla/maquettehandi/Adapters/HelpSomeoneInstantAdapter.java
+++ prototype/maquette-android/app/src/main/java/com/example/sikanla/maquettehandi/Adapters/HelpSomeoneInstantAdapter.java
@@ -1,6 +1,8 @@
 package com.example.sikanla.maquettehandi.Adapters;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +12,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sikanla.maquettehandi.DialogFragment.ProfileDialogFragment;
 import com.example.sikanla.maquettehandi.Model.InstantRequest;
 import com.example.sikanla.maquettehandi.Model.PlannedRequest;
 import com.example.sikanla.maquettehandi.R;
 import com.example.sikanla.maquettehandi.network.ImageRequester;
+import com.example.sikanla.maquettehandi.network.InstantRequester;
+import com.example.sikanla.maquettehandi.network.PlannedRequester;
 import com.example.sikanla.maquettehandi.network.UserRequester;
 import com.squareup.picasso.Picasso;
 
@@ -51,13 +56,13 @@ public class HelpSomeoneInstantAdapter extends ArrayAdapter<InstantRequest> {
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
-        InstantRequest id = getItem(position);
+        final InstantRequest id = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         final ViewHolder viewHolder; // view lookup cache stored in tag
         if (convertView == null) {
             // If there's no view to re-use, inflate a brand new view for row
             viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
+            final LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_answer_instant, parent, false);
             viewHolder.firstname = (TextView) convertView.findViewById(R.id.answer_instant_firstname);
             viewHolder.surname = (TextView) convertView.findViewById(R.id.answer_instant_surname);
@@ -66,7 +71,7 @@ public class HelpSomeoneInstantAdapter extends ArrayAdapter<InstantRequest> {
             // Cache the viewHolder object inside the fresh view
             convertView.setTag(viewHolder);
 
-            UserRequester  userRequester = new UserRequester();
+            UserRequester userRequester = new UserRequester();
             userRequester.getUser(context, id.getId(), new UserRequester.GetUserCB() {
                 @Override
                 public void getUser(String firstName, String surname, String age, Boolean success) {
@@ -78,6 +83,60 @@ public class HelpSomeoneInstantAdapter extends ArrayAdapter<InstantRequest> {
                 }
             });
 
+            viewHolder.pictureContact.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    loadProfile(id.id);
+                }
+            });
+
+            viewHolder.halpButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+                    builder.setMessage("Vous serez mis en contact, via la messagerie.\n" +
+                            "L'aidant sera notifié de votre réponse.")
+                            .setTitle("Aider cette personne?");
+
+
+                    final AlertDialog dialog = builder.create();
+                    dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Annuler", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Aider!",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    final InstantRequester instantRequester = new InstantRequester();
+                                    instantRequester.respondInstant(context.getApplicationContext(),
+                                            id.getId(), id.idInstant, new InstantRequester.InstantCB() {
+                                                @Override
+                                                public void onInstantCB(boolean success) {
+                                                    if (success) {
+                                                        instantRequests.remove(id);
+                                                        clear();
+                                                        addAll(instantRequests);
+                                                        notifyDataSetChanged();
+
+                                                        Toast.makeText(context, "Succès", Toast.LENGTH_LONG).show();
+                                                    } else {
+                                                        Toast.makeText(context, "ERREUR, REESSAYER", Toast.LENGTH_LONG).show();
+                                                    }
+                                                }
+                                            });
+                                    dialog.dismiss();
+                                }
+                            });
+
+                    dialog.show();
+                }
+
+            });
+
+
             ImageRequester imageRequester = new ImageRequester();
             imageRequester.getImage(id.getId(), context, new ImageRequester.ImageInterface() {
                 @Override
@@ -87,10 +146,20 @@ public class HelpSomeoneInstantAdapter extends ArrayAdapter<InstantRequest> {
 
                 }
             });
-        } else {
+        } else
+
+        {
             // View is being recycled, retrieve the viewHolder object from tag
             viewHolder = (ViewHolder) convertView.getTag();
         }
         return convertView;
+    }
+
+    private void loadProfile(String id) {
+        ProfileDialogFragment profileDialogFragment = new ProfileDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("id", id);
+        profileDialogFragment.setArguments(args);
+        profileDialogFragment.show(context.getFragmentManager(), "answerPlanned");
     }
 }
